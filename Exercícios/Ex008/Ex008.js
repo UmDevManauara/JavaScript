00f1c6cd3e5237e942a0de7378d60d2764f8c7c9
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-24 13:56:38 -0400

    criei Ex008

---
+++ Exercícios/Ex008/Ex008.js
@@ -0,0 +1,8 @@
+var vel = 61
+
+console.log(`A velocidade do seu carro é ${vel}KM/h`) 
+
+if (vel > 60) {// condição simples Não tem else
+    console.log(`Você ultrapassou a velociade permitida. MULTADO!`) 
+}
+console.log ( 'Dirija sempre usando cinto de segurança')
