c6267361334554609c4faa37d38e85cef64f0b73
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-25 10:54:51 -0400

    Terminei Ex 013 e 014

---
+++ Exercícios/Ex014/Ex014.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+   
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'manha.png'
+        document.body.style.background = '#ebbf8b'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'tarde.jpg'
+        document.body.style.background = '#b47037'
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#243350'
+    
+    }
+}
