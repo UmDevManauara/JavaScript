eed7c3825e07a527aaf6e5ea68f46c776c1e8c8f
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-26 11:44:58 -0400

    Criei o EX 016

---
+++ Exercícios/Ex016/Ex016.js
@@ -0,0 +1,62 @@
+function contar() {
+    var inicio = window.document.getElementById('iinicio')
+    var passo = window.document.getElementsByName('ipasso')
+    var fim = window.document.querySelector('input#ifim')
+    window.alert(`inicio: ${inicio} fim: ${fim} e passo: ${passo}`)
+    
+    for(c = Number(inicio.value); c <= Number(fim.value); c += Number(passo.value)){}
+
+    // var data = new Date()
+    // var ano = data.getFullYear()
+
+    // if (nasc.value.length == 0 || Number(nasc.value) > ano) {
+    //     window.alert('[EERO] Verifique novamente os dados e tente novamente!')
+    // } else {
+    //     var idade = ano - Number(nasc.value)
+    //     genero = ''
+    //     var img = document.createElement('img')
+    //     img.setAttribute('id', 'foto')
+    //     if (sexo[0].checked) {
+    //         genero = 'Homem'
+    //         if (idade >= 0 && idade <= 14) {
+    //             //CRIANÇA
+    //             img.setAttribute('src', 'imagens/m-crianca.png')
+    //         } else if (idade > 14 && idade < 18) {
+    //             // ADOLESCENTE
+    //             img.setAttribute('src', 'imagens/m-adolescente.png')
+    //         } else if (idade >= 18 && idade < 50) {
+    //             //Adulto
+    //             img.setAttribute('src', 'imagens/m-adulto.png')
+    //         } else if (idade >= 50 && idade < 70) {
+    //             // senhor
+    //             img.setAttribute('src', 'imagens/m-senhor.png')
+    //         } else {
+    //             //idoso
+    //             img.setAttribute('src', 'imagens/m-idoso.png')
+    //         }
+    //     } else {
+    //         genero = 'Mulher'
+    //         if (idade >= 0 && idade <= 14) {
+    //             //CRIANÇA
+    //             img.setAttribute('src', 'imagens/f-crianca.png')
+    //         } else if (idade > 14 && idade < 18) {
+    //             // ADOLESCENTE
+    //             img.setAttribute('src', 'imagens/f-adolescente.png')
+    //         } else if (idade >= 18 && idade < 50) {
+    //             //Adulto
+    //             img.setAttribute('src', 'imagens/f-adulta.png')
+    //         } else if (idade >= 50 && idade < 70) {
+    //             // senhor
+    //             img.setAttribute('src', 'imagens/f-senhora.png')
+    //         } else {
+    //             //idoso
+    //             img.setAttribute('src', 'imagens/f-idosa.png')
+    //         }
+            
+    //     }
+    //     res.style.textAlign = 'center'
+    //     res.innerHTML = `Detectamos ${genero} com idade de ${idade}`
+    //     res.appendChild(img)
+    }
+
+
--- Exercícios/While, Do While, For/teste.js
+++ Exercícios/While, Do While, For/teste.js
@@ -1,17 +1,21 @@
- var contador = 1
+// // While
+var contador = 1
  while (contador <= 2){
     console.log( `Passo ${contador}`)
     contador ++
  }
+ // //  DO WHILE
  contador2 = 1
 do{
-
 console.log(`Contador ${contador2}`)
 contador2++
 
- }while (contador2 <=3)
+}while (contador2 <=3)
  
+// FOR
 
+console.log('vai começar!')
 for(var c =1; c<=3; c++){
    console.log(`Carneirinhos ${c}`)
 }
+console.log('Fim!')
