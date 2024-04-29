0a1d1d752f9cf442da56c58bb88df3409c8d9181
Matheus Alves <164942311+UmDevManauara@users.noreply.github.com>
2024-04-29 10:51:29 -0400

    att

---
+++ Exercícios/Teste Ambiente/Array.js
@@ -0,0 +1,16 @@
+// para declarar um array, basta usar colchete
+let num = [5,8,4]
+console.log(num)
+
+// para inserir um dado em uma posição especifica do array basta indicar a posição
+num[3] = 6
+console.log(`array com elemento adicionado: ${num}`)
+
+//se quiser adicionar um elemento ao final do array, mas não quer declarar a posição
+num.push(7)
+console.log(`array com elemento adicionado ao final: ${num}`)
+//atributo length para retornar o comprimento começando por 1
+console.log(` A contagem de elementos atraves do atributo length, é igual a: `+ num.length)
+//metodo sort, ordena os valores em ordem
+num.sort()
+console.log(`o vetor ordenado pelo metodo Sort() é: ${num}`)
